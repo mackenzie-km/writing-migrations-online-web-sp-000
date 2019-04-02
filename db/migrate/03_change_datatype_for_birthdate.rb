1834efe655ca07a6d016f4151f5e2d9f3cb85c8f
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-02 21:51:20 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def change
-    change_table(:students, :birthdate, datetime)
+    change_table(:students, :birthdate, :datetime)
   end
 end
